5c8ce5b2e4c5c2ec12c413e47edc14d8d6a2e32b
wakhaew <megaaalisuper@gmail.com>
2024-03-19 15:34:25 +0300

    add gOAuth part 1

---
+++ app/src/main/java/com/example/finanse/expenses.java
@@ -0,0 +1,14 @@
+package com.example.finanse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class expenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
